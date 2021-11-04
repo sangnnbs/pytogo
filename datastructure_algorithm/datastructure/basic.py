3f276f78fde89fc76a6a46c9f3746981d3c95ae4
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-11-04 12:36:11 +0700

    datastructures

---
+++ datastructure_algorithm/datastructure/basic.py
@@ -0,0 +1,10 @@
+# Following the articles on checkme.txt
+# Let's focus on basic knowledge of Data Structures (ref: https://pythongeeks.org/python-data-structures/)
+# This cheatsheet would help (https://storage.googleapis.com/kaggle-forum-message-attachments/923535/16292/Python%20Data%20Structures%20Cheat-sheet.jpg)
+
+
+
+#--------------- Lists -------------
+# Create list
+list = []       # Syntax
+list = list()   # Constructor
