882eeefe28cfb195d8efd92bb8217a07ad9e7ebc
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-11-21 14:04:03 +0700

    update

--- oop/test2.py
+++ oop/test2.py
@@ -1,12 +1,25 @@
-import pickle 
+
+def listInc(list):                          # Space Complexity (0)= 1  ; Time Complexity (0) = n^2 
+
+    for i in range(0, len(list)):   
+        for j in range(i + 1, len(list)):  #------> range(i + 1, len(listA))
+            if list[j] > list[i]:
+                list[j], list[i] = list[i], list[j]
+                
+    return print(list)
 
 
-mylist = ['I wish to complain about this parrot what I purchased not half an hour ago from this very boutique.', "Oh yes, the, uh, the Norwegian Blue...What's,uh...What's wrong with it?", "I'll tell you what's wrong with it, my lad. 'E's dead, that's what's wrong with it!", "No, no, 'e's uh,...he's resting."]
-with open('vdv.pkl', 'ab') as f:
-    pickle.dump(mylist, f)
 
+def listDec(list):
 
-with open('vdv.pkl', 'rb') as f:
-    newlist = pickle.load(f)
+    for i in range(0, len(list)):
+        for j in range(i + 1, len(list)): #------> range(i + 1, len(listA))
+            if list[j] < list[i]:
+                list[j], list[i] = list[i], list[j]
+    return print(list)
 
-print(newlist)
+listA = [55,124,32,44,55,1,9]
+
+listInc(listA) #-- Hàm list tăng
+print('and')
+listDec(listA) #-- Hàm list giảm
