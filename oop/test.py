f4b9827249c9855b0312765db1e9189e22f524fc
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-11-24 13:58:25 +0700

    ok

--- oop/test.py
+++ oop/test.py
@@ -1,110 +1,20 @@
-import pickle
+import cgi, cgitb 
 
 
-class ThanhVien:
-    
-    # Bien Private
-
-    
-    # Khoi tao bien thanh vien
-    
-    def __init__(self, id, hoten, cannang, chieucao):
-        
-        self.id = id
-        self.__hoten = hoten
-        self.__cannang = cannang
-        self.__chieucao = chieucao
- 
-    
-    # Lấy Họ Tên
-    def GetHoTen(self):
-        return self.__hoten
-    
-    # Lấy Chiều cao
-    def GetChieuCao(self):
-        return self.__chieucao
-    
-    # Lấy Cân nặng
-    def GetCanNang(self):
-        return self.__cannang
-
-
-vandongvien = ThanhVien(None, None, None, None )
-
-list_vdv = []
-
-# File lưu thông tin VDV
-file_vdv = "./vdv.txt"
-
-#print(vandongvien.GetHoTen())
-#vandongvien.NhapThongTin()
-#vandongvien.ThemThongTin()
-#print(vandongvien.GetThongTin())
-#print(vandongvien.GetHoTen())
-
-
-
-
-#vandongvien = ThanhVien(    id = int(input('Nhập ID: ')), 
-#                            hoten = input('Nhập Họ Tên: '), 
-#                            cannang = int(input('Nhập cân nặng: ')), 
-#                            chieucao= int(input('Nhập chiều cao: '))
-#                        )
-
-
-
-while True:
-    khoidong = input('Hello, muốn làm gì: ')
-    # Them thanh vien
-    if khoidong == 'them':
-        vandongvien = ThanhVien(    id = int(input('Nhập ID: ')), 
-                                    hoten = input('Nhập Họ Tên: '), 
-                                    cannang = int(input('Nhập cân nặng: ')), 
-                                    chieucao= int(input('Nhập chiều cao: '))
-                        )
-        list_vdv.append(vandongvien)
-
-        # Nạp vandongvien
-
-        with open('vdv.pkl', 'wb') as w:
-            pickle.dump(list_vdv, w)
-
-        print(f'Thông tin vận động viên mới:{list_vdv[len(list_vdv) - 1]} có Họ Tên: {vandongvien.GetHoTen()}, chiều cao = {vandongvien.GetChieuCao()} , cân nặng = {vandongvien.GetCanNang()}')
-
-        # Xuất vandongvien
-        with open('vdv.pkl', 'rb') as r:
-            new_list = pickle.load(r)
-        
-        print(new_list) # ---------> Hiện list_vdv đã thêm vandongvien
-
-    # Xem max chieu cao
-    if khoidong == 'xem max cao':
-
-        with open('vdv.pkl', 'rb') as r:
-            new_list = pickle.load(r)
-        hoten = ''
-        a = 0
-        for obj in new_list:
-            if a < obj.GetChieuCao():
-                a = obj.GetChieuCao()
-                hoten = obj.GetHoTen()
-
-        print(f'Vận động viên cao nhất với {a} có họ tên {hoten}')
-    
-    # Xem max can nang
-    if khoidong == 'xem max nang':
-
-        with open('vdv.pkl', 'rb') as r:
-            new_list = pickle.load(r)
-        hoten = ''
-        b = 0
-        for obj in new_list:
-            if b < obj.GetCanNang():
-                b = obj.GetCanNang()  
-                hoten = obj.GetHoTen()
-
-        print(f'Vận động viên cao nhất với {b} có họ tên {hoten}')
-        
-    elif khoidong == 'exit':
-        print("..........")
-        break
+form = cgi.FieldStorage()      
+your_name = form.getvalue('your_name')    
+  
+# to get the data from fields
+company_name = form.getvalue('company_name')   
+  
+print ("Content-type:text/html\n")
+print ("<html>")
+print ("<head>")
+print ("<title>First CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello, %s is working in %s</h2>" 
+       % (your_name, company_name))
+  
+print ("</body>")
+print ("</html>")
--- oop/test2.py
+++ oop/test2.py
@@ -1,3 +1,4 @@
+import cgi, cgitb 
 
 def listInc(list):                          # Space Complexity (0)= 1  ; Time Complexity (0) = n^2 
 
@@ -23,3 +24,22 @@ listA = [55,124,32,44,55,1,9]
 listInc(listA) #-- Hàm list tăng
 print('and')
 listDec(listA) #-- Hàm list giảm
+
+
+
+# Tách chuỗi string
+
+text = 'dump v.duration n.dynamic adj.eager adj.economics n.economist n.editorial adj.efficiently adv'
+
+def TachChuoi(a):
+    x = a.split('.')
+    c = ''
+    b = []
+    for i in x:
+        c = i.split(' ')
+        b.append(c[0])
+            
+    for i in range(0,len(b)):
+        print(b[i])
+
+TachChuoi(text)
